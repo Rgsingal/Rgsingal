3e1ca21e680c5e763c9d5370b0ccdb6442b70713
Rgsingal <162784107+Rgsingal@users.noreply.github.com>
2024-08-07 12:18:31 -0700

    Add files via upload

---
+++ #built a menu.py
@@ -0,0 +1,6 @@
+#built a menu
+title = "Menu".upper()
+print(title.center(20,"="))
+print("Coffee".ljust(20,".") + "$1".rjust(4))
+print("Pakora".ljust(22,".") + "$1.50".rjust(4))
+print("custard".upper().ljust(20,".") + "$2".rjust(4))
